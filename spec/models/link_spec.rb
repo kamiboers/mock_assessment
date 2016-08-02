2c27b66bd3e1d350fc1469c9e5a5cd7faca64146
Kami Boers <kamiboers@gmail.com>
2016-08-02 11:32:23 -0600

    add user validations

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Link, type: :model do
 
-   context "validations" do
+  context "validations" do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:url) }
     it { is_expected.to validate_uniqueness_of(:url) }
